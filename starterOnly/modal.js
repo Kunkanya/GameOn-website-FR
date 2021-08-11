dca9bc79ff3a789cc1ee198310218e6c2f703548
Kunkanya Remmer <kunkanyaremmer@hotmail.com>
2021-08-11 13:03:47 +0200

    V5: edited function setInputError and function setInputSuccess

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,17 +13,18 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 //Kunkanya set variable
-const form = document.getElementsById("reserve");
+const form = document.getElementById("form");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthDate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
-const formSubmit =document.getElementsByClassName("btn-submit");
+const y = document.querySelector("btn-submit");
 
 
 //Kunkanya : Error messages for moDAL VALIDATION
 const nameErrorMessage = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+const emailErrorMessage = "Veuillez entrer valid E-mail address.";
 const birthDateErrorMessage = "Vous devez entrer votre date de naissance.";
 
 // launch modal event
@@ -39,52 +40,75 @@ function closeModal() {
   modalbg.style.display = "none";
 };
 
-form.addEventListener('submit', e =>{
-  e.preventDefault();
-  console.log("prevent default");
-  validate();
+//Kunkanya : add preventdefault() for input form when the page is loading 
+form.addEventListener('submit', (event)=>{
+  event.preventDefault();
+  //validate();
 });
 
+//Kunkanya : function validation all the input forms before click submit button
 function validate(){
-  checkInput();
-  return true;
-};
-
-function checkInput(){
-    // check Firstname and trimming value from firstname
-    if (firstName.value == "" || firstName.value.length < 2){
-        setClassError(firstName, nameErrorMessage);
-        firstName.focus;
-    }else{
-        setClassSuccess(firstName);
-    }
-    if (lastName.value == "" || lastName.value.length < 2){
-      setClassError(lastName, nameErrorMessage);
-      lastName.focus;
-    }else{
-      setClassSuccess(firstName);
+  // Kunkanya: check Input for firstname if it is valid
+  if(firstName.value === "" || firstName.value.length < 2){
+    console.log("tryagain");
+    // Kunkanya: when error go to setInputError function
+    setInputError(firstName , nameErrorMessage);
+    ;
+  }else{
+    // Kunkanya: when success go to setInputError function
+    setInputSuccess(firstName);
   }
-};
 
-// set class error to .text-control function
-function setClassError(input, message){
-  input.className = 'text-control error';
-  const p = document.getElementsByClassName("errMsg");
-  p.innerHTML = message
-  //alert(p.innerHTML.value);
-};
+  //Kunkanya: check Lastname
+    if(lastName.value === "" || lastName.value.length < 2){
+      console.log("tryagain");
+      // Kunkanya: when error go to setInputError function
+      setInputError(lastName , nameErrorMessage);
+    }else{
+      // Kunkanya: when success go to setInputError function
+      setInputSuccess(lastName);
+    }
 
-// set class to success to .text-control function
-function setClassSuccess(input){
-  input.className = 'text-control';
-};
+    //Kunkanya: check E-mail
+  if(email.value === "" || email.value == null){
+    console.log("tryagain email");
+    // Kunkanya: when error go to setInputError function
+    setInputError(email , emailErrorMessage);
+  }else{
+    // Kunkanya: when success go to setInputError function
+    setInputSuccess(email);
+  }
 
-// try and catch error
-try {
-  validate();
-} catch (error) {
-  console.log(error instanceof TypeError);
-  alert(error.message + " " + 
-        error.lineNumber );
-  
+  //Kunkanya: check birthdate
+   if(birthDate.value === "" ){
+    console.log("tryagain birthdate");
+    // Kunkanya: when error go to setInputError function
+    setInputError(birthDate, birthDateErrorMessage);
+  }else{
+    // Kunkanya: when success go to setInputError function
+    setInputSuccess(birthDate);
+  }
+
+  };
+
+//Kunkanya: if error, input = HTMLElement we want to check and message is the error message 
+function setInputError(input, message){
+  //Kunkanya : show the border red when input Error
+  input.style.border = "solid red 2px";
+  //Kunkanya : set the parent element in this case shold be "formData"
+  let parent = input.parentElement;
+  //Kunkanya : select the element for error message by using parentElement
+  let err = parent.querySelector('small');
+  //Kunkanya: show errorMessage block with red color and texts
+  err.style.display = "block";
+  err.style.color ="red";
+  err.innerText = message;
+}
+
+//Kunkanya: InputSuccess : set border to green and display "none" for the error message
+function setInputSuccess(input){
+  input.style.border = "solid green 2px";
+  let parent = input.parentElement;
+  let err = parent.querySelector('small');
+  err.style.display = "none";
 }
